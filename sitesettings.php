95a3917fe33513ab7f65ecaca855cbd56173285c
matthewycso <matthewycso@gmail.com>
2015-12-27 21:35:12 +0800

    lang detect

--- sitesettings.php
+++ sitesettings.php
@@ -1,7 +1,19 @@
 <?php
+$supportedLangs = array('en', 'zh');
+$languages = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+$lang_accept_main = "en";
+foreach($languages as $lang_accept)
+{
+    if(in_array(substr($lang_accept, 0, 2), $supportedLangs))
+    {
+        // Set the page locale to the first supported language found
+        $lang_accept_main = substr($lang_accept, 0, 2);
+        break;
+    }
+}
 
 // Get requested language 
-$lang = !isset($_REQUEST["lang"]) ? "en" : $_REQUEST["lang"] ;
+$lang = !isset($_REQUEST["lang"]) ? $lang_accept_main : $_REQUEST["lang"] ;
 
 // Load language specific files
 $msg_file =  "i18n/messages-".$lang.".php"; 
